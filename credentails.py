44c3031f27a39d2c04eeaefaa77b549f6a294b58
Amruth Lal <amruth27m@gmail.com>
2017-07-10 00:29:31 +0530

    Add credentials maker

---
+++ credentails.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#program to write data to credentials file
+#Written by Amruthlal M
+#amruth27m@gmail.com
+from Crypto.Cipher import ARC4
+from json import dumps
+
+dataDictonary = { '<Your data here as key value pair>'}
+dataDictonaryJSON = dumps(ls)
+dataDictonaryEncrypted = ARC4.new('<your key here>').encrypt(dataDictonaryJSON)
+with open('credentials.dat','wb') as f:
+	f.write(dataDictonaryEncrypted)
+
