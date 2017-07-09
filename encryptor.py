2e3540ea7975b8e9bb152a0f2be033543e5001a9
Amruth Lal <amruth27m@gmail.com>
2017-07-09 15:03:05 +0530

    Adds an Encryptor for securing the private data

---
+++ encryptor.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#Program to decrypt and modify the data variables stored
+#Written by Amruthlal M
+#amruth27m@gmail.com
+
+from Crypto.Cipher import ARC4
+from json import  dumps,loads
+
+class Encryptor:
+	def __init__(self):
+		self._key = '<your-key-here>'
+
+	def decryptCredentials(self):
+		try:
+			with open('credentials.dat','rb') as file:
+				EncryptedData = file.read()
+		except FileNotFoundError:
+			print('Fatal error! Credentials file not found. Contact admin')
+			return
+		DecryptedData = ARC4.new(self._key).decrypt(EncryptedData).decode('latin-1')
+		return loads(DecryptedData)
+
+	def encryptCredentials(self,data):
+		jsonData = dumps(data)
+		EncryptedData = ARC4.new(self._key).encrypt(jsonData)
+		try:
+			with open('credentials.dat','wb') as file:
+				file.write(EncryptedData)
+				return True
+		except:
+			pass
+			
+	def getData(self):
+		return self.decryptCredentials()
